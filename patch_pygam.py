e8c774e6ff7b36d497cbf45c7966498952998429
nicolocavalli <nicolo.cavalli@unibocconi.it>
2025-05-01 22:02:25 +0200

    Update patch_pygam.py

--- patch_pygam.py
+++ patch_pygam.py
@@ -1,11 +1,12 @@
 # patch_pygam.py
 import numpy as np
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, csc_matrix
 
 # Fix deprecated np.int
 if not hasattr(np, 'int'):
     np.int = int
 
-# Restore .A attribute for scipy csr_matrix
-if not hasattr(csr_matrix, 'A'):
-    csr_matrix.A = property(lambda self: self.toarray())
+# Restore .A attribute on csr_matrix and csc_matrix
+for sparse_type in [csr_matrix, csc_matrix]:
+    if not hasattr(sparse_type, 'A'):
+        sparse_type.A = property(lambda self: self.toarray())
